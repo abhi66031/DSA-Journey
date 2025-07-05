f7110f3078ff2496705a3737fd4bfd564279e6a6
Abhishek choudhary <acabhishek63@gmail.com>
2025-07-06 00:41:39 +0530

    .

--- .vscode/ARRAYS/ARRAYS.java
+++ .vscode/ARRAYS/ARRAYS.java
@@ -1,11 +1,6 @@
-// Take an array as input from the user
-// search for a given number x 
-// and  print the index at which  it occurs.
-// LINEAR SEARCH(DSA QUESTION)
-
 import java.util.Scanner;
 
-class abhi{
+public class ARRAYS{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
@@ -26,11 +21,10 @@ class abhi{
 
         // match the number and throw the result
 
-        for(int i=0;i<=size;i++){
+        for(int i=0;i<size;i++){
             if (number[i]==x) {
                 System.out.println("matched at index"+i);   
             }  
     
         }    
     }
-}
