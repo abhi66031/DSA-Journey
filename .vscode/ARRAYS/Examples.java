95339ea6ccf42d4de56a6c8399a678528ab8d122
Abhishek choudhary <acabhishek63@gmail.com>
2025-07-06 19:35:27 +0530

    /

---
+++ .vscode/ARRAYS/Examples.java
@@ -0,0 +1,6 @@
+public class Examples {
+    public static void main(String[] args) {
+        System.out.println("hello world");// Example 1: Basic Array Declaration and Initialization
+        }
+}
+
